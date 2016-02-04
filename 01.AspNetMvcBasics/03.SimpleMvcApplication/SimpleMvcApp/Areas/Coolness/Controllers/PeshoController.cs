3fa0ead7743ed519f51c19db0eaca2d379624859
V.Dinev <vassildinev@gmail.com>
2016-02-04 18:20:45 +0200

    MVC basics homework fix

---
+++ 01.AspNetMvcBasics/03.SimpleMvcApplication/SimpleMvcApp/Areas/Coolness/Controllers/PeshoController.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleMvcApp.Areas.Coolness.Controllers
+{
+    using System.Web.Mvc;
+
+    public class PeshoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
